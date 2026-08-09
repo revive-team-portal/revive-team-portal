5c3358d2cda6c6e9323756f041eca13c307a1012
Jeremy Dixon <jeremy@revive.co.nz>
2026-08-09 20:32:09 +1200

    POS report: add Trading days (distinct days with sales>0) after Transactions

--- netlify/functions/_posreport.js
+++ netlify/functions/_posreport.js
@@ -11,21 +11,23 @@ function reportSql(start, endNext) {
     " FROM EJItemsTable i JOIN EJTable t ON t.Transaction_Number=i.Transaction_Number LEFT JOIN ProductTable p ON p.Inventory_Code=i.InventoryCode WHERE " + W +
     " UNION ALL SELECT 'txn', NULL, NULL, NULL, NULL, CAST(COUNT(DISTINCT t.Transaction_Number) AS int)" +
     " FROM EJItemsTable i JOIN EJTable t ON t.Transaction_Number=i.Transaction_Number WHERE " + W +
+    " UNION ALL SELECT 'tradedays', NULL, NULL, NULL, NULL, CAST((SELECT COUNT(*) FROM (SELECT CAST(t.Receipt_Date_Time AS date) AS dd FROM EJItemsTable i JOIN EJTable t ON t.Transaction_Number=i.Transaction_Number WHERE " + W + " GROUP BY CAST(t.Receipt_Date_Time AS date) HAVING SUM(i.Sales) > 0) z) AS int)" +
     " UNION ALL SELECT 'media', CAST(m.MediaType AS varchar(10)), CAST(MAX(md.MediaDescription) AS varchar(64)), CAST(SUM(m.MediaAmount) AS decimal(18,2)), NULL, CAST(COUNT(DISTINCT m.Transaction_Number) AS int)" +
     " FROM EJMediaTable m JOIN EJTable t ON t.Transaction_Number=m.Transaction_Number LEFT JOIN MediaDescriptionTable md ON md.MediaNumber=m.MediaType WHERE " + W + " AND m.MediaType < 100 GROUP BY m.MediaType HAVING SUM(m.MediaAmount) <> 0" +
     " ORDER BY 1, 4 DESC;";
 }
 function parseReport(tsv) {
   const lines = String(tsv || '').replace(/\r/g, '').trim().split('\n'); const cols = lines[0].split('\t'); const ix = c => cols.indexOf(c);
-  const depts = [], media = []; let covers = 0, txns = 0;
+  const depts = [], media = []; let covers = 0, txns = 0, tradeDays = 0;
   for (const ln of lines.slice(1)) { const r = ln.split('\t'); const sec = r[ix('section')];
     if (sec === 'dept') { const g = Number(r[ix('code')]); depts.push({ label: DEPTNAMES[g] || ('Group ' + g), amount: Number(r[ix('amount')] || 0), qty: Number(r[ix('qty')] || 0) }); }
     else if (sec === 'cover') covers = Number(r[ix('qty')] || 0);
     else if (sec === 'txn') txns = Number(r[ix('txns')] || 0);
+    else if (sec === 'tradedays') tradeDays = Number(r[ix('txns')] || 0);
     else if (sec === 'media') media.push({ label: r[ix('label')] || ('Media ' + r[ix('code')]), amount: Number(r[ix('amount')] || 0), txns: Number(r[ix('txns')] || 0) });
   }
   depts.sort((a, b) => b.amount - a.amount); media.sort((a, b) => b.amount - a.amount);
-  return { depts, deptTotal: depts.reduce((s, d) => s + d.amount, 0), deptQty: depts.reduce((s, d) => s + d.qty, 0), covers, txns, media, mediaTotal: media.reduce((s, d) => s + d.amount, 0) };
+  return { depts, deptTotal: depts.reduce((s, d) => s + d.amount, 0), deptQty: depts.reduce((s, d) => s + d.qty, 0), covers, txns, tradeDays, media, mediaTotal: media.reduce((s, d) => s + d.amount, 0) };
 }
 const money = n => '$' + (Number(n) || 0).toLocaleString('en-NZ', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 const num = n => (Number(n) || 0).toLocaleString('en-NZ');
@@ -44,6 +46,7 @@ function emailHtml(label, d) {
   h += '<tr><td ' + totl + '>Total sales</td><td ' + tot + '>' + num(d.deptQty) + '</td><td ' + tot + '>' + money(d.deptTotal) + '</td></tr>';
   h += '<tr><td ' + tdl + '>Covers (salads + meals)</td><td ' + td + '>' + num(d.covers) + '</td><td ' + td + '></td></tr>';
   h += '<tr><td ' + tdl + '>Transactions</td><td ' + td + '>' + num(d.txns) + '</td><td ' + td + '></td></tr>';
+  h += '<tr><td ' + tdl + '>Trading days</td><td ' + td + '>' + num(d.tradeDays) + '</td><td ' + td + '></td></tr>';
   h += '</tbody></table>';
   h += '<h3 style="margin:0 0 6px">By media type</h3>';
   h += '<table style="border-collapse:collapse;width:100%"><thead><tr><th ' + thl + '>Media</th><th ' + th + '>Count</th><th ' + th + '>Amount</th></tr></thead><tbody>';
